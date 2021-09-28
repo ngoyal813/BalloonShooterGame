15eb4981efa9db2c9204cf87b6e2481133069011
Nipun Goel <goelnipun2525@gmail.com>
2021-09-28 22:22:47 +0530

    game

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    
+    public Transform[] spawnPoints;
+    public GameObject[] balloons;
+
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+    }
+
+    IEnumerator StartSpawning(){
+
+        yield return new WaitForSeconds(4);
+
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(balloons[i],spawnPoints[i].position,Quaternion.identity);
+        }
+
+        StartCoroutine(StartSpawning());
+    }
+}
